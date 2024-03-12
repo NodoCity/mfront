aa9310b6ef47c13bb138663770615888e60b79d5
asistentekali <asistentekali1@gmail.com>
2024-03-12 13:55:39 -0500

    menu_lateral

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,82 @@
-// import { useState } from 'react'
-import { Layout } from 'antd'
+import React, { useState, useCallback } from 'react';
+import { Button, Collapse, Layout, theme } from 'antd';
+import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import Logo from './components/Logo';
 import MenuList from './components/MenuList';
+import ToggleThemeButton from './components/ToggleThemeButton';
+import ReactFlow, { MiniMap, Controls, Background, useNodesState, useEdgesState, addEdge } from 'reactflow';
+import 'reactflow/dist/style.css';
 
-const {Header, Sider}=Layout;
+const { Header, Sider } = Layout;
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
 function App() {
+  const [darkTheme, setDarkTheme] = useState(true);
+  const [collapsed, setCollapsed] = useState(false);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-   
     <Layout>
-      <Sider className='sidebar'>
-      <Logo/>
-      <MenuList />
+      <Sider
+        collapsed={collapsed}
+        collapsible
+        trigger={null}
+        theme={darkTheme ? 'dark' : 'light'}
+        className='sidebar'
+      >
+        <Logo />
+        <MenuList darkTheme={darkTheme} />
+        <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme} />
       </Sider>
-      </Layout> 
-  
-  )
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type='text'
+            className='toggle'
+            onClick={() => setCollapsed(!collapsed)}
+            setCollapsed
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          />
+        </Header>
+        <div style={{ width: '100%', height: 'calc(100vh - 64px)' }}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+          >
+            <Controls />
+            <MiniMap
+              style={{ width: 150, height: 100, position: 'absolute', right: 10, top: 10, borderRadius: 5, boxShadow: '0 0 10px rgba(0,0,0,0.3)' }}
+            />
+            <Background
+              style={{ backgroundColor: '#222', padding: 20, borderRadius: 5, boxShadow: '0 0 10px rgba(0,0,0,0.3)', position: 'absolute', bottom: 10, right: 10 }}
+            />
+          </ReactFlow>
+        </div>
+      </Layout>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -1,18 +1,23 @@
 import { Menu } from 'antd';
-import { HomeOutlined, AppstoreOutlined, AreaChartOutlined, PayCircleOutlined, SettingOutlined } from '@ant-design/icons';
+import { HomeOutlined, AppstoreOutlined, AreaChartOutlined, PayCircleOutlined, SettingOutlined, BarsOutlined } from '@ant-design/icons';
 
-const MenuList = () => {
+const MenuList = ({darkTheme}) => {
     return (
-        <Menu theme="dark">
+        <Menu theme={darkTheme? 'dark':'light'} mode='inline' className="menu-bar">
             <Menu.Item key="home" icon={<HomeOutlined />} >
                 Home
             </Menu.Item>
             <Menu.Item key="activity" icon={<AppstoreOutlined />} >
                 Activity
             </Menu.Item>
-            <menu.SubMenu>
-                
-            </menu.SubMenu>
+            <Menu.SubMenu key="tasks" icon={<BarsOutlined/>} title="Tasks" >
+                <Menu.Item key="task-1" >Tasks 1</Menu.Item>
+                <Menu.Item key="task-2" >Tasks 2</Menu.Item>
+                <Menu.SubMenu key="subtasks" title="subTasks" >
+                <Menu.Item key="subtask-1" >SubTasks 1</Menu.Item>
+                <Menu.Item key="subtask-2" >SubTasks 2</Menu.Item>
+                </Menu.SubMenu>
+            </Menu.SubMenu>
             <Menu.Item key="progress" icon={<AreaChartOutlined />} >
                 Progress
             </Menu.Item>
